2cac75264e921e5aafd57933e798194a15c14ae9
Toleranzy <cergand07@mail.ru>
2026-09-20 22:49:15 +0300

    name_patronymic

--- main1.cpp
+++ main1.cpp
@@ -2,9 +2,9 @@
 using namespace std;
 
 int main(){
-    cout << "Пожалуйста, введите свое имя и возраст(затем нажмите 'enter'):\n";
-    string first_name = "???";
-    int age = -1;
-    cin >> first_name >> age;
-    cout << "Hello, " << first_name << " (age " << age << ")!\n";
+    cout << "Пожалуйста, введите свое имя и отчество(затем нажмите 'enter'):\n";
+    string first;
+    string second;
+    cin >> first >> second;
+    cout << "Hello, " << first << ' ' << second << '\n';
 }
