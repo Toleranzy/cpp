aebde64992f348d235c09b25a3f299763999f05c
Toleranzy <cergand07@mail.ru>
2026-09-20 22:45:22 +0300

    warning

--- main1.cpp
+++ main1.cpp
@@ -3,9 +3,8 @@ using namespace std;
 
 int main(){
     cout << "Пожалуйста, введите свое имя и возраст(затем нажмите 'enter'):\n";
-    string first_name;
-    int age;
-    cin >> first_name;
-    cin >> age;
+    string first_name = "???";
+    int age = -1;
+    cin >> first_name >> age;
     cout << "Hello, " << first_name << " (age " << age << ")!\n";
 }
