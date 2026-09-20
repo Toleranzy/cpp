5463ce3f73b7cc0cbfb39c120beec2dfe3efba11
Toleranzy <cergand07@mail.ru>
2026-09-20 23:12:50 +0300

    operators

--- main1.cpp
+++ main1.cpp
@@ -1,11 +1,17 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
 
 int main(){
-    cout << "Пожалуйста, введите свой возраст(затем нажмите 'enter'):\n";
-    double first;
-    double second;
-    cin >> first;
-    second = first * 12;
-    cout << "Hello, ваш возраст в месяцах: " << second << "\n";
+    cout << "Пожалуйста, введите значение с плавающей точкой: ";
+    double n;
+    cin >> n;
+    cout << "n == " << n
+         << "\nn+1 == " << n+1
+         << "\nтри раза по n == " << 3*n
+         << "\nдва раза по n == " << n+n
+         << "\nn в квадрате == " << n*n
+         << "\nполовина n== "   << n/2
+         << "\nквадратный корень из n == " << sqrt(n)
+         << endl;
 }
