519e0b4dc451008f786302d13daadc8465c796f0
Toleranzy <cergand07@mail.ru>
2026-09-20 22:22:20 +0300

    3_chapter

---
+++ main1.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    cout << "Пожалуйста, введите ваше имя (затем нажмите 'enter'):\n";
+    string first_name;
+    cin >> first_name;
+    cout << "Hello, " << first_name << "!\n";
+}
