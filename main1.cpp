320d365978a0db1e24ada9ff4b4ae3ced911e5b2
Toleranzy <cergand07@mail.ru>
2026-09-26 16:13:38 +0300

    last

--- main1.cpp
+++ main1.cpp
@@ -3,13 +3,13 @@
 using namespace std;
 
 int main(){
-    cout << "Введите два имени\n";
-    string first;
-    string second;
-    cin >> first >> second;
-    if (first == second) cout << "имена совпадают\n";
-    if (first < second)
-        cout << first << " по алфавиту предшествует " << second << endl;
-    if (first > second)
-        cout << first << " по алфавиту следует за " << second << endl;
+    string previous = '';
+
+    string current;
+    while (cin>>current){
+        if (previous == current)
+
+        cout << "повторяющееся слово: " << current << '\n';
+        previous = current;
+    }
 }
