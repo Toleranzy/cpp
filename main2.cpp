7c34dd268186935e332e28925ebdf7c3147d2df0
Toleranzy <cergand07@mail.ru>
2026-09-26 18:13:52 +0300

    yandex2

--- main2.cpp
+++ main2.cpp
@@ -3,16 +3,7 @@
 #include <sstream>
 
 int main(){
-    std::string c;
-    std::getline(std::cin, c);
-
-    std::stringstream ss(c);
-    double number;
-    double sum = 0;
-
-    while (ss >> number){
-        sum += number;
-    }
-
-    std::cout << sum << "\n";
+    long long a,b;
+    std::cin >> a >> b;
+    std::cout << a + b << "\n";
 }
