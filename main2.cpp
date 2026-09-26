1a4157d44d121a460ea654cd0484cc51d933359f
Toleranzy <cergand07@mail.ru>
2026-09-26 18:33:12 +0300

    yandex4

--- main2.cpp
+++ main2.cpp
@@ -1,7 +1,14 @@
 #include <iostream>
 
-int main(){
-    double a;
-    std::cin >> a;
-    std::cout << a / 2.54 ;
+int main() {
+    long long n;
+    std::cin >> n;
+
+    long long sum;
+    if (n%2==0)
+        sum = n / 2 * (n+1);
+    else
+        sum = ((n+1)/2) * n ;
+
+    std::cout << sum << "\n";
 }
